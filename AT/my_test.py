1ff3f39cd2f2d55955c12301411c1aed547f2521
chenboluo <48652819+chenboluo@users.noreply.github.com>
2024-01-02 21:49:22 +0800

    Add files via upload

---
+++ AT/my_test.py
@@ -0,0 +1,182 @@
+from __future__ import print_function
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '0,1'
+import argparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.autograd import Variable
+
+
+
+
+from resnet import *
+import numpy as np
+import time
+
+from advertorch.attacks import LinfPGDAttack, CarliniWagnerL2Attack, DDNL2Attack, SinglePixelAttack, LocalSearchAttack, SpatialTransformAttack,L1PGDAttack
+import torchvision.transforms.functional as VF
+from torchattacks import PGD , AutoAttack, CW,EOTPGD
+from advertorch.utils import predict_from_logits
+from torchattacks import PGD , AutoAttack, CW,FGSM,OnePixel,Square,APGD,FAB
+
+
+
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
+
+parser = argparse.ArgumentParser(description='PyTorch CIFAR MART Defense')
+parser.add_argument('--batch-size', type=int, default=128, metavar='N',
+                    help='input batch size for training (default: 128)')
+parser.add_argument('--test-batch-size', type=int, default=100, metavar='N',
+                    help='input batch size for testing (default: 100)')
+parser.add_argument('--epochs', type=int, default=160, metavar='N',
+                    help='number of epochs to train')
+parser.add_argument('--weight-decay', '--wd', default=3.5e-3,
+                    type=float, metavar='W')
+parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
+                    help='learning rate')
+parser.add_argument('--momentum', type=float, default=0.9, metavar='M',
+                    help='SGD momentum')
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='disables CUDA training')
+parser.add_argument('--epsilon', default=0.031,
+                    help='perturbation')
+parser.add_argument('--num-steps', default=10,
+                    help='perturb number of steps')
+parser.add_argument('--step-size', default=0.007,
+                    help='perturb step size')
+parser.add_argument('--beta', default=5.0,
+                    help='weight before kl (misclassified examples)')
+parser.add_argument('--seed', type=int, default=1, metavar='S',
+                    help='random seed (default: 1)')
+parser.add_argument('--log-interval', type=int, default=100, metavar='N',
+                    help='how many batches to wait before logging training status')
+parser.add_argument('--model', default='TA4',
+                    help='directory of model for saving checkpoint')
+parser.add_argument('--save-freq', '-s', default=1, type=int, metavar='N',
+                    help='save frequency')
+
+args = parser.parse_args()
+cnt = 0
+    
+use_cuda = not args.no_cuda and torch.cuda.is_available()
+torch.manual_seed(1)
+device = torch.device("cuda" if use_cuda else "cpu")
+kwargs = {'num_workers': 10, 'pin_memory': True} if use_cuda else {}
+torch.backends.cudnn.benchmark = True
+
+# setup data loader
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+])
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+])
+trainset = torchvision.datasets.CIFAR10(root='../data_attack/', train=True, download=True, transform=transform_train)
+train_loader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size, shuffle=True, num_workers=10)
+testset = torchvision.datasets.CIFAR10(root='../data_attack/', train=False, download=True, transform=transform_test)
+test_loader = torch.utils.data.DataLoader(testset, batch_size=args.test_batch_size, shuffle=False, num_workers=10)
+
+
+
+
+
+from torchvision import utils as vutils
+
+
+
+
+
+
+
+class modelx(nn.Module):
+    def __init__(self):
+        super(modelx,self).__init__()
+        self.model = ResNet18().cuda() 
+        predmodel = torch.load('')  
+        self.model.load_state_dict(predmodel)
+     
+    def forward(self,imgs):    
+        imgs = imgs.cuda()
+        pre1 = self.model(imgs)
+        return pre1
+        
+
+       
+#model2 = get_resnet()       
+model = modelx()
+
+
+
+
+
+
+
+
+def main():
+
+
+    natural_acc = []
+    robust_acc = []
+    cnt = 0
+    logits = [0, 0, 0, 0, 0]
+    for nat_data, labels in test_loader:
+        nat_data, labels = nat_data.cuda(), labels.cuda()
+        start_time = time.time()
+
+        model.eval()
+
+        adversary = LinfPGDAttack(model, loss_fn=nn.CrossEntropyLoss(reduction="sum"), eps=8/255,nb_iter=40, eps_iter=0.01, rand_init=True, clip_min=0.0, clip_max=1.0, targeted=False)
+        adv = adversary.perturb(nat_data, labels)
+        
+        adversary = DDNL2Attack(model, nb_iter=40, gamma=0.05, init_norm=1.0, quantize=True, levels=256, clip_min=0.0,
+                            clip_max=1.0, targeted=False, loss_fn=None)
+        adv2 = adversary(nat_data, labels)
+
+        attack = CW(model, c=1, kappa=0, steps=500, lr=0.01)
+        adv3 = attack(nat_data, labels)
+        
+        adversary = SpatialTransformAttack(model, 10, clip_min=0.0, clip_max=1.0, max_iterations=10, search_steps=5, targeted=False)
+        adv4 = adversary(nat_data, labels)
+
+        
+        
+        
+        data = [nat_data,adv,adv2,adv3,adv4]
+
+
+        cnt = cnt + labels.shape[0]
+
+
+
+        for j in range(5):
+            imgs = data[j]
+            model.eval()
+            
+            imgs = imgs.cuda()
+            pre1 = model(imgs)
+            pred = predict_from_logits( pre1 )
+            for i in range(len(pred)):
+                 if(  pred[i]==labels[i] ):
+                     logits[j] = logits[j]+1
+
+    for i in range(5):
+        print(logits[i])
+    print(cnt)
+
+
+
+
+
+if __name__ == '__main__':
+    main()
+    
+    
+    
+   
+
---
+++ Ours/resnet.py
@@ -0,0 +1,147 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion * planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion * planes)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, self.expansion * planes, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(self.expansion * planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion * planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion * planes)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = F.relu(self.bn2(self.conv2(out)))
+        out = self.bn3(self.conv3(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+class Separation(torch.nn.Module):
+    def __init__(self, size, num_channel=64, tau=0.1):
+        super(Separation, self).__init__()
+        C, H, W = size
+        self.C, self.H, self.W = C, H, W
+        self.tau = tau
+
+        self.sep_net = nn.Sequential(
+            nn.Conv2d(C, num_channel, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(num_channel),
+            nn.ReLU(),
+            nn.Conv2d(num_channel, num_channel, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(num_channel),
+            nn.ReLU(),
+            nn.Conv2d(num_channel, C, kernel_size=3, stride=1, padding=1, bias=False)
+        )
+
+    def forward(self, feat, is_eval=False):
+        l = self.sep_net(feat)
+        h = feat - l
+        mask = torch.rand(feat.shape).cuda()
+        mask = mask>0.01
+        mask2 = torch.rand(feat.shape).cuda()
+        mask2 = mask2>0.1
+        feat = l*mask + h*mask2
+        return feat
+
+
+
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(ResNet, self).__init__()
+        self.in_planes = 64
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.linear = nn.Linear(512 * block.expansion, num_classes)
+        self.separation4 = Separation(size=(512, 4, 4))
+        self.separation3 = Separation(size=(256, 8, 8))
+        self.separation2 = Separation(size=(128, 16, 16))        
+        self.separation1 = Separation(size=(64, 32, 32))                    
+        #self.separation0 = Separation(size=(3, 32, 32))                
+        
+        
+        
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1] * (num_blocks - 1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        #x = self.separation0(x)        
+        out = F.relu(self.bn1(self.conv1(x)))
+        
+        out = self.layer1(out)
+        #print(out.shape)
+        out = self.separation1(out)
+        
+        out = self.layer2(out)
+        #print(out.shape)        
+        out = self.separation2(out)  
+        
+        
+        out = self.layer3(out)
+        #print(out.shape)                
+        #out = self.separation3(out)        
+       
+        out = self.layer4(out)
+        #print(out.shape)        
+        out = self.separation4(out)        
+        
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        #exit(0)
+        return out
+
+
+def ResNet18():
+    return ResNet(BasicBlock, [2, 2, 2, 2])
+
+def ResNet50():
+    return ResNet(Bottleneck, [3, 4, 6, 3])
